e2a58b2537a328e4e8e1e4dbdfb7eddaacc88b60
Pau Ferrer <pau.ferrer@monkeyminds.io>
2025-09-12 15:31:38 +0200

    Fixing on resize issue...[Created init() function to extract responsibilities]

--- lists/load-more/index.js
+++ lists/load-more/index.js
@@ -16,39 +16,37 @@ window.onload = () => {
     const list = document.querySelector('[mm-list-element="list"]')
     const moreButton = document.querySelector('[mm-list-element="more-button"]')
     const overlay = document.querySelector('[mm-list-element="overlay"]') || null
-
-    // Variables
-    let rowHeight = list.children[0].offsetHeight + 32
-    let rowsCount = Math.ceil(list.parentElement.offsetHeight / rowHeight) - 1
-    let initHeight = (rowHeight + (overlay ? rowHeight * 0.75 : 0))
-    let clicksCount = 0
-    console.log("Initial Row Height: ", rowHeight)
-    console.log("Initial Row Count: ", rowsCount)
-    console.log("Initial Init Height: ", initHeight)
     // Functions
-    const setContainerHeight = () => initHeight + (rowHeight * clicksCount) + 'px'
+    const setContainerHeight = (height, row, clicks) => height + (row * clicks) + 'px'
     const setFinalState = () => {
         overlay && removeElement(overlay)
         moreButton && removeElement(moreButton)
         return 'auto'
     }
-    // Init state
-    list.parentElement.style.height = setContainerHeight()
+    // Variables
+    let rowHeight = 0
+    let rowsCount = 0
+    let initHeight = 0
+    let clicksCount = 0
+    // Initial setup
+    const init = () => {
+        rowHeight = list.children[0].offsetHeight + 32
+        rowsCount = Math.ceil(list.parentElement.offsetHeight / rowHeight) - 1
+        initHeight = (rowHeight + (overlay ? rowHeight * 0.75 : 0))
+        list.parentElement.style.height = setContainerHeight(initHeight, rowHeight, clicksCount)
+    }
+    init()
+    console.log("Initial Row Count: ", rowsCount)
     // Responsiveness
     window.onresize = () => {
-        rowHeight = list.children[0].offsetHeight + 32
-        rowsCount = Math.ceil(list.parentElement.offsetHeight / rowHeight)
-        initHeight = (rowHeight + (overlay ? rowHeight * 0.75 : 0))
-        list.parentElement.style.height = setContainerHeight()
-        console.log("Row Height: ", rowHeight)
+        init()
         console.log("Row Count: ", rowsCount)
-        console.log("Init Height: ", initHeight)
     }
     // Click animation
     moreButton.onclick = (event) => {
         event.preventDefault()
         clicksCount++
         if(clicksCount === rowsCount) list.parentElement.style.height = setFinalState()
-        else list.parentElement.style.height = setContainerHeight()
+        else list.parentElement.style.height = setContainerHeight(initHeight, rowHeight, clicksCount)
     }
 }
